88e971b17ad01d35757a4409f769f242739a330e
AntiKiljoy <hstconnor@gmail.com>
2024-10-22 13:49:42 +0200

    -
    cry

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Nav from "./components/Nav";
 import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
+import CreatePage from "./pages/CreatePage";
 import ContactPage from "./pages/ContactPage";
 
 function App() {
-    return (
-        <>
-            <Nav />
-            <main>
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/about" element={<AboutPage />} />
-                    <Route path="/contact" element={<ContactPage />} />
-                    <Route path="*" element={<Navigate to="/" />} />
-                </Routes>
-            </main>
-        </>
-    );
+  return (
+    <>
+      <Nav />
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<CreatePage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </main>
+    </>
+  );
 }
 
 export default App;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
+import PostCard from "../components/PostCard";
+
 export default function HomePage() {
-    return (
-        <section className="page">
-            <h1>Home Page</h1>
-            <p>Home is where the heart is 💛</p>
-            <p>Oh My, sounds like a bad movie!</p>
-        </section>
-    );
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetchPosts();
+
+    async function fetchPosts() {
+      const url =
+        "https://react-user-crud-app-1cfaa-default-rtdb.europe-west1.firebasedatabase.app/posts.json";
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      const postsArray = Object.keys(data).map((postId) => ({
+        id: postId,
+        ...data[postId],
+      }));
+      console.log(postsArray);
+      setPosts(postsArray);
+    }
+
+    fetchPosts();
+  }, []);
+  return (
+    <section className="page">
+      <div className="grid">
+        {posts.map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </div>
+    </section>
+  );
 }
